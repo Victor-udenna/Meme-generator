c120de1180de0630944ef2dab974d1c361733f54
victor okwuosa <victorudenna@gmail.com>
2023-02-03 01:11:10 +1300

    Edited meme.js file

--- src/components/Meme/Meme.js
+++ src/components/Meme/Meme.js
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Data from '../../Data/Data'
+
+
 
  const Meme = () => {
+
+  const [memeImage, setmemeImage] = useState("https://i.imgflip.com/1g8my4.jpg")
+
+
+const handleClick = ()=>{
+    const memesArray = Data.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setmemeImage(memesArray[randomNumber].url)
+}
+
+
+
+
   return (
    <main className='bg-green-400 p-10'>
-    <form className='grid grid-cols-2 gap-4'>
+    <div className='grid grid-cols-2 gap-4'>
         <input type="text"
         placeholder='top-text'
         className='rounded-md p-3'/>
         <input type="text"
         placeholder='bottom-text'
         className='rounded-md p-3'/>
-        <button className='col-start-1 col-end-4 bg-red-500 btn rounded-md p-5 pionter'>Get a new meme image  😀</button>
-    </form>
+        <button className='col-start-1 col-end-4 bg-red-500 btn rounded-md p-5 pionter' onClick={handleClick}>Get a new meme image  😀</button>
+    </div>
+
+    <div>
+      <img className='w-[300px] h-[300px]' src={memeImage}/>
+    </div>
    </main>
   )
 }
